11e7627b0797440f462a835ade6cf54e0b34292e
cyanidepanda <cyanide@panda-lang.org>
2016-05-03 22:36:04 +0200

    Added Lily object.

--- src/lily.js
+++ src/lily.js
@@ -1,23 +1,48 @@
-// Initialize
-var textArea = q('#lily-editor');
-textArea.remove();
 
-var lilyWorkspace = e('.lily-workspace');
-var lineNumbers = e('.lily-line-numbers');
-var editor = e('.lily-editor');
+function Lily() {
+    // Initialize
+    var lily = this;
+    var lilyEditor = q('.lily-editor');
+    var lilyEditorParent = lilyEditor.parentNode;
 
-editor.contentEditable = true;
-editor.on('click', function () {
-    rs();
-});
+    this.lilyWorkspace = e('div.lily-workspace');
+    this.lineNumbers = e('div.lily-line-numbers');
+    this.textArea = e('textarea.lily-textarea');
+    this.editor = e('div.lily-editor');
 
-parse();
+    // Editor
+    this.editor.contentEditable = true;
+    this.editor.on('click', function () {
+        lily.rs();
+    });
 
-lilyWorkspace.append(editor);
-lilyWorkspace.append(lineNumbers);
-textArea.parentElement.append(lilyWorkspace);
+    // Parse content
+    this.parse();
 
-function rs() {
+    // Apply changes
+    lilyEditor.remove();
+    this.lilyWorkspace.append(this.editor);
+    this.lilyWorkspace.append(this.lineNumbers);
+    lilyEditorParent.append(this.lilyWorkspace);
+}
+
+Lily.prototype.parse = function() {
+    var content = this.getContent();
+    var lines = content.split('\n');
+
+    this.lineNumbers.html('');
+    for (var i = 1; i < lines.length + 1; i++) {
+        var lineNumber = e('p.lily-line-number');
+        lineNumber.html(i);
+        this.lineNumbers.append(lineNumber);
+    }
+
+    this.editor.html(content);
+    this.rs();
+};
+
+// Functions
+Lily.prototype.rs = function () {
     var range = document.createRange();
     var sel = window.getSelection();
     try {
@@ -27,27 +52,15 @@ function rs() {
         sel.addRange(range);
     } catch (exception) {
     }
-}
+};
+
+Lily.prototype.getContent = function () {
+    return this.editor.text().replace('<br><br>', '\n');
+};
 
 function getCharByKeyCode(keyCode) {
     return String.fromCharCode((96 <= keyCode && keyCode <= 105) ? keyCode - 48 : keyCode);
 }
 
-function parse() {
-    var content = getContent();
-    var lines = content.split('\n');
-
-    lineNumbers.html('');
-    for (var i = 1; i < lines.length + 1; i++) {
-        var lineNumber = e('p.lily-line-number');
-        lineNumber.append(t(i));
-        lineNumbers.append(lineNumber);
-    }
-
-    editor.html(content);
-    rs();
-}
-
-function getContent(){
-    return editor.text().replace('<br><br>', '\n');
-}
+// Run
+var lily = new Lily();
